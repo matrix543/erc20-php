2c888242fec45af875bffb75db3f28f1fc0a44bb
Matrix <christophergidlof@gmail.com>
2022-12-07 11:53:02 +0100

    Update ERC20_Token.php

--- src/ERC20_Token.php
+++ src/ERC20_Token.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * This file is a part of "furqansiddiqui/erc20-php" package.
- * https://github.com/furqansiddiqui/erc20-php
+ * This file is a part of "matrix543/erc20-php" package.
+ * https://github.com/matrix543/erc20-php
  *
  * Copyright (c) 2020 Furqan A. Siddiqui <hello@furqansiddiqui.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code or visit following link:
- * https://github.com/furqansiddiqui/erc20-php/blob/master/LICENSE
+ * https://github.com/matrix543/erc20-php/blob/master/LICENSE
  */
 
 declare(strict_types=1);
