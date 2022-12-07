79f1f6ff9fa780304fe8f91b66d4a7ca43dd28f0
Matrix <christophergidlof@gmail.com>
2022-12-07 11:56:00 +0100

    Update ERC20Exception.php

--- src/Exception/ERC20Exception.php
+++ src/Exception/ERC20Exception.php
@@ -7,7 +7,7 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code or visit following link:
- * https://github.com/furqansiddiqui/erc20-php/blob/master/LICENSE
+ * https://github.com/matrix543/erc20-php/blob/master/LICENSE
  */
 
 declare(strict_types=1);
